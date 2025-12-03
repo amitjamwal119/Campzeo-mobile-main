dcd3f3662f40dc6d88639c4b8686e19f7d7a86e0
Amit Jamwal <ak7719869@gmail.com>
2025-12-03 14:13:16 +0530

    Amit's Ui changes

--- app/(calander)/calanderPage.tsx
+++ app/(calander)/calanderPage.tsx
@@ -1,24 +1,37 @@
 import { ThemedText } from "@/components/themed-text";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CalendarComponent from "../(common)/calenderComponent";
-
-
-
+import { HStack } from "@gluestack-ui/themed";
+import { Pressable } from "@gluestack-ui/themed";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 export default function CalendarScreen() {
- 
+  const routePage = useRouter();
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       {/* Header */}
-      <ThemedText
-        style={{ fontSize: 30, lineHeight: 36, fontWeight: 700 }}
-        className="text-center mt-5 mb-9"
-      >
-        Scheduled Campaigns
-      </ThemedText>
 
-      {/* 📅 Calendar */}
-      <CalendarComponent/>
+      
+      <HStack className="px-3 pt-5 gap-3 items-center">
+        <Pressable
+          onPress={() => {
+            routePage.back();
+          }}
+        >
+          <Ionicons name="arrow-back-outline" size={22} color="#334155" />
+        </Pressable>
+        <ThemedText
+          style={{ fontSize: 20, lineHeight: 36, fontWeight: 700 }}
+          className="text-center"
+        >
+          Scheduled Campaigns
+        </ThemedText>
+      </HStack>
+
+      {/*  Calendar */}
+      <CalendarComponent />
     </SafeAreaView>
   );
 }
